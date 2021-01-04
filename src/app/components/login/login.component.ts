a63714a622040f9d09000470d9d474b8c38a14b7
Cristian Gomez <gomez.garcia.cristian@outlook.com>
2021-01-04 08:23:41 -0600

    Creacion de componente login

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterExtensions } from '@nativescript/angular';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  users = [
+    {
+      name: 'Cristian',
+      address: 'Cuajimalpa',
+      nacionality: 'Mexicana'
+    },
+    {
+      name: 'Itzel',
+      address: 'Huejutla',
+      nacionality: 'Mexicana'
+    },
+    {
+      name: 'Carlos',
+      address: 'Monterrey',
+      nacionality: 'Mexicano'
+    },
+    {
+      name: 'Roxelle',
+      address: 'Waukegan',
+      nacionality: 'EUA'
+    },
+    {
+      name: 'Pedro',
+      address: 'Barcelona',
+      nacionality: 'EUA'
+    }
+  ];
+
+  constructor(
+    private routerExtensions: RouterExtensions
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  onNavItemTap(NavItemTap: String) {
+    this.routerExtensions.navigate([NavItemTap], {
+      transition: {
+        name: 'fade'
+      }
+    })
+  }
+
+}
